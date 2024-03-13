5e3662dcc8edd6a709f7da4355129411e1a1280b
aparrab90 <aparrab90@gmail.com>
2024-03-13 17:03:26 -0500

    Creación de que la clase que se encarga de crear y configurar un DataSource para conectar con PostgreSQL

---
+++ src/main/java/org/acme/camelIntegracionFueradeLineaaparrab/Components/AppDataSource.java
@@ -0,0 +1,16 @@
+package org.acme.camelIntegracionFueradeLineaaparrab.Components;
+
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class AppDataSource {
+
+    public static DataSource createDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://postgres:5432/postgres");
+         dataSource.setUsername("camel");
+        dataSource.setPassword("camel");
+        return dataSource;
+    }
+}
