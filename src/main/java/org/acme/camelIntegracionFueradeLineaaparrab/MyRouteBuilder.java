3cfe8f2302626203c13286c50899821185060b27
Angel Javier Parra Bagua <aparrab90@gmail.com>
2024-03-14 13:29:32 -0500

    --

--- src/main/java/org/acme/camelIntegracionFueradeLineaaparrab/MyRouteBuilder.java
+++ src/main/java/org/acme/camelIntegracionFueradeLineaaparrab/MyRouteBuilder.java
@@ -45,21 +45,9 @@ public class MyRouteBuilder extends RouteBuilder {
             .split(body()).streaming()
                 .process(new ValidatorProcessor(successfulRecords, recordCounter))
                 .process(exchange -> {
-<<<<<<< HEAD
-                    ClientCreditProfile payment = exchange.getIn().getBody(ClientCreditProfile.class);
-                    String insertSQL = String.format(
-                        "INSERT INTO public.customer_payments " +
-                        "(id) VALUES " +
-                        "(%d)",
-                        payment.getId().intValue()
-                        
-                    );
-                    
-=======
                     CustomerPayment payment = exchange.getIn().getBody(CustomerPayment.class);
                     String insertSQL = String.format("INSERT INTO customer_payments (id, limit_bal, sex, education, marriage, age, pay_0_1, pay_2_1, pay_3_1, pay_4_1, pay_5_1, pay_6_1, pay_1_2, pay_2_2, pay_3_2, pay_4_2, pay_5_2, pay_6_2, bill_1, bill_2, bill_3, bill_4, bill_5, bill_6, default_payment_next_month) VALUES (%d, %f, %d, %d, %d, %d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %d)",
                             payment.getID().intValue(), payment.getLIMIT_BAL().doubleValue(), payment.getSEX().intValue(), payment.getEDUCATION().intValue(), payment.getMARRIAGE().intValue(), payment.getAGE().intValue(), payment.getPAY_0_1().doubleValue(), payment.getPAY_2_1().doubleValue(), payment.getPAY_3_1().doubleValue(), payment.getPAY_4_1().doubleValue(), payment.getPAY_5_1().doubleValue(), payment.getPAY_6_1().doubleValue(), payment.getPAY_1_2().doubleValue(), payment.getPAY_2_2().doubleValue(), payment.getPAY_3_2().doubleValue(), payment.getPAY_4_2().doubleValue(), payment.getPAY_5_2().doubleValue(), payment.getPAY_6_2().doubleValue(), payment.getBILL_1().doubleValue(), payment.getBILL_2().doubleValue(), payment.getBILL_3().doubleValue(), payment.getBILL_4().doubleValue(), payment.getBILL_5().doubleValue(), payment.getBILL_6().doubleValue(), payment.getDefaultPaymentNextMonth().intValue());
->>>>>>> a22e340692f368488c4223da792fedbd7b85a373
                     exchange.getIn().setBody(insertSQL);
                 })
                 .to("jdbc:myDataSource") // Usa el DataSource para insertar en la base de datos
