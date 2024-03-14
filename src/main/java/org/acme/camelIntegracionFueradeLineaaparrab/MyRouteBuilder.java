a22e340692f368488c4223da792fedbd7b85a373
aparrab90 <aparrab90@gmail.com>
2024-03-14 13:01:13 -0500

    --

--- src/main/java/org/acme/camelIntegracionFueradeLineaaparrab/MyRouteBuilder.java
+++ src/main/java/org/acme/camelIntegracionFueradeLineaaparrab/MyRouteBuilder.java
@@ -1,14 +1,14 @@
 package org.acme.camelIntegracionFueradeLineaaparrab;
-import org.apache.camel.builder.RouteBuilder;
+
 import java.util.concurrent.atomic.AtomicInteger;
 import javax.sql.DataSource;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
-import org.acme.camelIntegracionFueradeLineaaparrab.Components.AppDataSource;
-import org.acme.camelIntegracionFueradeLineaaparrab.Components.ClientCreditValidationProcessor;
-import org.acme.camelIntegracionFueradeLineaaparrab.Components.RecordValidationException;
-import org.acme.camelIntegracionFueradeLineaaparrab.Components.ClientCreditProfile;
+import org.acme.camelIntegracionFueradeLineaaparrab.Components.DataSourceConfig;
+import org.acme.camelIntegracionFueradeLineaaparrab.Components.ValidatorProcessor; 
+import org.acme.camelIntegracionFueradeLineaaparrab.Components.ValidationException;
+import org.acme.camelIntegracionFueradeLineaaparrab.Components.CustomerPayment;
 
 
 public class MyRouteBuilder extends RouteBuilder {
@@ -22,13 +22,13 @@ public class MyRouteBuilder extends RouteBuilder {
     public void configure() {
 
         // Registrar el DataSource creado por DataSourceConfig
-        DataSource dataSource = AppDataSource.createDataSource();
+        DataSource dataSource = DataSourceConfig.createDataSource();
         getContext().getRegistry().bind("myDataSource", dataSource);
         
         recordCounter.set(0); // Reiniciar contador de registros
 
         // Manejo de excepciones general para errores de validación
-        onException(RecordValidationException.class)
+        onException(ValidationException.class)
             .process(exchange -> {
                 failedRecords.incrementAndGet(); // Incrementar contador de fallidos
                 Exception exception = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
@@ -37,52 +37,17 @@ public class MyRouteBuilder extends RouteBuilder {
             .log("${body}")
             .handled(true);
 
-        BindyCsvDataFormat bindyFormat = new BindyCsvDataFormat(ClientCreditProfile.class);
+        BindyCsvDataFormat bindyFormat = new BindyCsvDataFormat(CustomerPayment.class);
 
-        //from("sftp://localhost:22/upload?username=foo&password=pass&noop=true")
         from("sftp://sftp:22/upload?username=foo&password=pass&noop=true")
-        //from("sftp://localhost:22/upload?username=foo&password=pass&useUserKnownHostsFile=false&strictHostKeyChecking=no")
             .routeId("read-csv-file-route")
             .unmarshal(bindyFormat)
             .split(body()).streaming()
-                .process(new ClientCreditValidationProcessor(successfulRecords, recordCounter))
+                .process(new ValidatorProcessor(successfulRecords, recordCounter))
                 .process(exchange -> {
-                    ClientCreditProfile payment = exchange.getIn().getBody(ClientCreditProfile.class);
-                    String insertSQL = String.format(
-                        "INSERT INTO public.customer_payments " +
-                        "(id, limit_bal, sex, education, marriage, age, " +
-                        "pay0, pay2, pay3, pay4, pay5, pay6, " +
-                        "bill_amt1, bill_amt2, bill_amt3, bill_amt4, bill_amt5, bill_amt6, " +
-                        "pay_amt1, pay_amt2, pay_amt3, pay_amt4, pay_amt5, pay_amt6, " +
-                        "default_payment_next_month) VALUES " +
-                        "(%d, %f, %d, %d, %d, %d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %d)",
-                        payment.getId().intValue(),
-                        payment.getLimitBal().doubleValue(),
-                        payment.getSex().intValue(),
-                        payment.getEducation().intValue(),
-                        payment.getMarriage().intValue(),
-                        payment.getAge().intValue(),
-                        payment.getPay0().doubleValue(),
-                        payment.getPay2().doubleValue(),
-                        payment.getPay3().doubleValue(),
-                        payment.getPay4().doubleValue(),
-                        payment.getPay5().doubleValue(),
-                        payment.getPay6().doubleValue(),
-                        payment.getBillAmt1().doubleValue(),
-                        payment.getBillAmt2().doubleValue(),
-                        payment.getBillAmt3().doubleValue(),
-                        payment.getBillAmt4().doubleValue(),
-                        payment.getBillAmt5().doubleValue(),
-                        payment.getBillAmt6().doubleValue(),
-                        payment.getPayAmt1().doubleValue(),
-                        payment.getPayAmt2().doubleValue(),
-                        payment.getPayAmt3().doubleValue(),
-                        payment.getPayAmt4().doubleValue(),
-                        payment.getPayAmt5().doubleValue(),
-                        payment.getPayAmt6().doubleValue(),
-                        payment.getDefaultPaymentNextMonth().intValue()
-                    );
-                    
+                    CustomerPayment payment = exchange.getIn().getBody(CustomerPayment.class);
+                    String insertSQL = String.format("INSERT INTO customer_payments (id, limit_bal, sex, education, marriage, age, pay_0_1, pay_2_1, pay_3_1, pay_4_1, pay_5_1, pay_6_1, pay_1_2, pay_2_2, pay_3_2, pay_4_2, pay_5_2, pay_6_2, bill_1, bill_2, bill_3, bill_4, bill_5, bill_6, default_payment_next_month) VALUES (%d, %f, %d, %d, %d, %d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %d)",
+                            payment.getID().intValue(), payment.getLIMIT_BAL().doubleValue(), payment.getSEX().intValue(), payment.getEDUCATION().intValue(), payment.getMARRIAGE().intValue(), payment.getAGE().intValue(), payment.getPAY_0_1().doubleValue(), payment.getPAY_2_1().doubleValue(), payment.getPAY_3_1().doubleValue(), payment.getPAY_4_1().doubleValue(), payment.getPAY_5_1().doubleValue(), payment.getPAY_6_1().doubleValue(), payment.getPAY_1_2().doubleValue(), payment.getPAY_2_2().doubleValue(), payment.getPAY_3_2().doubleValue(), payment.getPAY_4_2().doubleValue(), payment.getPAY_5_2().doubleValue(), payment.getPAY_6_2().doubleValue(), payment.getBILL_1().doubleValue(), payment.getBILL_2().doubleValue(), payment.getBILL_3().doubleValue(), payment.getBILL_4().doubleValue(), payment.getBILL_5().doubleValue(), payment.getBILL_6().doubleValue(), payment.getDefaultPaymentNextMonth().intValue());
                     exchange.getIn().setBody(insertSQL);
                 })
                 .to("jdbc:myDataSource") // Usa el DataSource para insertar en la base de datos
